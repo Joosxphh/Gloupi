40dd7be021979f1208b3000c7e2f4041789de853
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-02-13 17:40:25 +0100

    fix: package.json

--- app/(main)/add/camera.tsx
+++ app/(main)/add/camera.tsx
@@ -1,53 +1,47 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { Camera, CameraType } from "expo-camera";
-import { useRouter } from "expo-router";
-import { useEffect, useRef, useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useMeals } from '../../../context/MealContext';
+import { useRouter } from 'expo-router';
 
-export default function CameraScreen() {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(false);
-    const [type, setType] = useState<CameraType>("back");
-    const cameraRef = useRef(null);
+export default function App() {
+    const [permission, requestPermission] = useCameraPermissions();
+    const { addMeal } = useMeals();
     const router = useRouter();
 
-    useEffect(() => {
-        (async () => {
-            const { status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === "granted");
-        })();
-    }, []);
-
-    const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-        router.back();
-    };
-
-    if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>;
+    if (!permission) {
+        return <View />;
     }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function handleBarCodeScanned() {
+        console.log('Barcode scanned:');
+        fetch(`https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.EXPO_PUBLIC_EDAMAM_APP_ID}&app_key=${process.env.EXPO_PUBLIC_EDAMAM_PUBLIC_KEY}&upc=${data}`)
+            .then(response => response.json())
+            .then(result => {
+                console.log('API response:', result); // Log the API response
+                if (result.hints && result.hints.length > 0) {
+                    const foodItem = result.hints[0].food;
+                    addMeal([foodItem]);
+                    router.push('/add');
+                } else {
+                    console.error('No food item found for the scanned barcode');
+                }
+            })
+            .catch(error => console.error('Error fetching food item:', error));
     }
 
     return (
         <View style={styles.container}>
-            <Camera
-                ref={cameraRef}
-                style={StyleSheet.absoluteFillObject}
-                type={type}
-                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            />
-            {scanned && (
-                <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-            )}
-            <TouchableOpacity
-                style={styles.closeButton}
-                onPress={() => router.back()}
-            >
-                <MaterialIcons name="close" size={36} color="white" />
-            </TouchableOpacity>
+            <CameraView style={styles.camera} onBarCodeScanned={handleBarCodeScanned} />
         </View>
     );
 }
@@ -55,17 +49,29 @@ export default function CameraScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: 'center',
     },
-    closeButton: {
-        position: "absolute",
-        top: 50,
-        right: 20,
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
     },
 });
